6c875b354ef6b30f33c8240a3e0e83af1f7bccae
matthewhodgin <matthew.a.hodgin@bryanuniversity.edu>
2022-04-25 20:16:08 -0700

    Week2

--- Week2/calendar.js
+++ Week2/calendar.js
@@ -1,54 +1,63 @@
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 for (i = 0; i < months.length; i++){   
-    document.getElementById('month').options[i] = new Option (months[i], i+1)
+    if (new Date().getMonth() == i)
+        document.getElementById('month').options[i] = new Option (months[i], i+1, false, true)
+    else
+        document.getElementById('month').options[i] = new Option (months[i], i+1)
 }
+
+document.querySelector('#year').value = new Date().getFullYear()
+
 
 document.getElementById('printCalendar').addEventListener("click", function(){printCalendar()})
 function printCalendar(){
-document.getElementById('calendarDays').innerHTML = ""
-const firstday = `${document.querySelector('#month').value}/1/${document.querySelector('#year').value}`
+    document.getElementById('calendarDays').innerHTML = ""
+    const firstday = `${document.querySelector('#month').value}/1/${document.querySelector('#year').value}`
 
-const today = new Date(firstday)
-const month = document.querySelector('#month').value
+    const today = new Date(firstday)
+    const month = document.querySelector('#month').value
 
-let days
-switch (month-1) {
-    case 1:
-        days = 28
-        break
-    case 3:
-    case 5:
-    case 8: 
-    case 10:
-        days = 30
-        break
-    default:
-        days = 31
-}
- 
-let x
-const weekday = today.getDay()
-for (x = 0; x < weekday; x++){
-    document.getElementById('calendarDays').innerHTML += "<div class='blankDay'>&nbsp;</div>"
-}
+    let days
+    switch (month-1) {
+        case 1:
+            days = 28
+            break
+        case 3:
+        case 5:
+        case 8: 
+        case 10:
+            days = 30
+            break
+        default:
+            days = 31
+    }
+    
+    let x
+    const weekday = today.getDay()
+    for (x = 0; x < weekday; x++){
+        document.getElementById('calendarDays').innerHTML += "<div class='blankDay'>&nbsp;</div>"
+    }
 
-let dt = 0
-do {
-    dt++
-    document.getElementById('calendarDays').innerHTML += `<div class='calendarCell'>${dt}</div`
-} while ( dt < days)
+    let dt = 0
+    do {
+        dt++
+        document.getElementById('calendarDays').innerHTML += `<div class='calendarCell'>${dt}</div`
+    } while ( dt < days)
 
-const monthName = today.toLocaleDateString('default', {month:'long'})
-const year = today.getFullYear()
-document.querySelector('.calendarTitle').innerText = `${monthName} ${year}`
+    const monthName = today.toLocaleDateString('default', {month:'long'})
+    const year = today.getFullYear()
+    document.querySelector('.calendarTitle').innerText = `${monthName} ${year}`
 
-const remainder = (7 - ((x + dt) % 7))
-let y = 0
-while ( y < remainder) {
-    document.getElementById('calendarDays').innerHTML += "<div class='blankDay'>&nbsp;</div>"
-    y++
-}
-}
+    const remainder = (7 - ((x + dt) % 7))
+    let y = 0
+    while ( y < remainder) {
+        document.getElementById('calendarDays').innerHTML += "<div class='blankDay'>&nbsp;</div>"
+        y++
+    }
+    }
+
+printCalendar()
+
 
 // modify this script to populate the month select you create in the HTML page from an array of month names
 // you can use either a for loop or an array.map to populate the select. remember that while arrays start with 
